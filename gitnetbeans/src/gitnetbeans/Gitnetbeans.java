e28ab86034c6346079f1b89d725a678e7703c4e5
ferranfb <96294366+ferranfb@users.noreply.github.com>
2022-01-14 19:38:05 +0100

    comentario

--- gitnetbeans/src/gitnetbeans/Gitnetbeans.java
+++ gitnetbeans/src/gitnetbeans/Gitnetbeans.java
@@ -1,4 +1,4 @@
-/*
+/*Ejercicios de Arrays
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
